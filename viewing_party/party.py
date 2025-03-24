c9f9edce170218a6e9336ef3590780dab5ba86b5
Shirley425 <tanqiaoqiao425@gmail.com>
2025-03-24 16:13:32 -0700

    Save progress wave 01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,37 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    #Create a data structure for each movie
+    movie = {}
+
+    #Check if title, genre, rating parameters are TRUE
+    if title and genre and rating != None:
+        movie["title"] = title
+        movie["genre"] = genre
+        movie["rating"] = rating
+        return movie 
+
+    #Check if title, genre, rating parameters are FALSE
+    else:
+        return None
+
+def add_to_watched(user_data, movie):
+    user_data = {}
+    #Create a "watched" list
+    watched = []
+
+    #Set the "watched" key to the list of watched movies as a value
+    user_data["watched"] = watched
+
+    #Add movie to the watched list
+    watched.append(movie)
+
+    return user_data
+
+
+
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
