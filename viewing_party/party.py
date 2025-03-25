c62e65dde80de5514b32b27f37de13ca3742b6ff
Shirley425 <tanqiaoqiao425@gmail.com>
2025-03-24 19:03:49 -0700

    wave01 update, still need to debug funtion4

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -16,20 +16,51 @@ def create_movie(title, genre, rating):
         return None
 
 def add_to_watched(user_data, movie):
-    user_data = {}
-    #Create a "watched" list
-    watched = []
-
-    #Set the "watched" key to the list of watched movies as a value
-    user_data["watched"] = watched
-
+    #Check if "watched" key exists:
+    if "watched" not in user_data:
+        #Set the "watched" key to the list of watched movies as a value
+        user_data["watched"] = []  
     #Add movie to the watched list
-    watched.append(movie)
+    user_data["watched"].append(movie)
+    return user_data
 
+def add_to_watchlist(user_data, movie):
+    #Add movie to watchlist inside of user_data
+    if "watchlist" not in user_data:
+        user_data["watchlist"] = movie
+
+    user_data["watchlist"].append(movie)
     return user_data
 
 
+janes_data = {
+    "watchlist": [{
+        "title": "MOVIE_TITLE_1",
+        "genre": "GENRE_1",
+        "rating": 4.5
+    }],
+    "watched": []
+}
 
+def watch_movie(user_data, title):
+    #Iterate through user_data dictionary by its keys
+    for _ in user_data["watchlist"]:
+        #If the title is in a movie in the user's watchlist
+        if title in user_data["watchlist"]:
+            print(f"THE MOVIE TITLE IS: ***** ",{title})
+            #add that movie to watched
+            #user_data["watched"].append(title)
+            
+            #remove that movie from the watchlist
+            user_data["watchlist"].remove(title)
+
+            return user_data
+        else:
+            return user_data
+        
+
+result = watch_movie(janes_data, "MOVIE_TITLE_1")
+print(result)
 
 
 
