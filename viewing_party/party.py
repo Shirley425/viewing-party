14cdafebfc762514bacb4dcf792e7ff47096d200
Shirley425 <tanqiaoqiao425@gmail.com>
2025-03-25 15:27:45 -0700

    Implements wave 02 functions. All tests passing.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -38,7 +38,6 @@ def watch_movie(user_data, title):
     watchlist = user_data["watchlist"]
     watched_movies = user_data["watched"]
 
-    #print(watchlist[0]["title"])
     for item in watchlist:
         if item["title"] == title:
             #Remove item of type dict from watchlist of type list
@@ -50,12 +49,54 @@ def watch_movie(user_data, title):
     return user_data
 
 
-
-
-
-# -----------------------------------------
 # ------------- WAVE 2 --------------------
-# -----------------------------------------
+def get_watched_avg_rating(user_data):
+    watched_movies = user_data["watched"]
+    sum_rating = 0
+    count = 0
+
+    
+    #In case when watched_movies is an empty list
+    if watched_movies == []:
+        return 0.0
+    # Iterate through list of dicts called watched_movies
+    else:
+        for movie in watched_movies:
+            sum_rating += movie["rating"]
+            count += 1
+    # Access the rating key in that dict
+    # Sum every rating and calculate average
+    
+        return sum_rating/count
+
+def get_most_watched_genre(user_data):
+    watched_movies = user_data["watched"]
+    genre_dict = {}
+
+    #In case when watched_movies is an empty list
+    if watched_movies == []:
+        return None
+    # Iterate through list of dicts called watched_movies
+    else:
+        for movie in watched_movies:
+            if movie["genre"] not in genre_dict:
+                genre_dict[movie["genre"]] = 1
+            else:
+                genre_dict[movie["genre"]] += 1
+
+    print(genre_dict)
+
+    #Get the key with the maximum value 
+    most_watched_genre_count = 0
+    most_watched_genre = ""
+
+    # Iterate through list of dicts called genre_dict
+    for genre in genre_dict.keys():
+        if genre_dict[genre] > most_watched_genre_count:
+            most_watched_genre_count = genre_dict[genre]
+            most_watched_genre = genre
+    return most_watched_genre
+
 
 
 # -----------------------------------------
