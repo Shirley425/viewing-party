7edf103151e427677421c973cbd79c2c2a5292cb
Shirley425 <tanqiaoqiao425@gmail.com>
2025-03-27 00:12:46 -0700

    wave_03 finished, still need to update test_wave_03 third one only.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -98,12 +98,43 @@ def get_most_watched_genre(user_data):
     return most_watched_genre
 
 
-
-# -----------------------------------------
 # ------------- WAVE 3 --------------------
-# -----------------------------------------
+def get_unique_watched(user_data):
+    user_watched_list = user_data["watched"]
+    friends_list = user_data["friends"]
+    
+    friends_watched_set = set()
+    unique_watched = []
 
-        
+    #Iterate through the movies in nested friends_list
+    for items in friends_list:
+        for movie in items["watched"]:  
+            if movie in user_watched_list:
+                friends_watched_set.add(movie["title"])
+
+    #Iterate through the movies in user_watched_list
+    for movie in user_watched_list:
+        if movie["title"] not in friends_watched_set:
+            unique_watched.append(movie)
+    return unique_watched
+
+def get_friends_unique_watched(user_data):
+    user_watched_list = user_data["watched"]
+    friends_list = user_data["friends"]
+    
+    user_watched_set = set()
+    unique_watched = []
+
+    for movie in user_watched_list:
+        user_watched_set.add(movie["title"])
+
+    for items in friends_list:
+        for movie in items["watched"]:  
+            if movie["title"] not in user_watched_set and movie not in user_watched_list:
+                unique_watched.append(movie)
+    return unique_watched
+    
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
